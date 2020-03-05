a4a7ae4f36b3976591cadf8a024c577e0b13301e
mdu02 <32987869+mdu02@users.noreply.github.com>
2020-03-05 10:39:22 -0500

    asdf

--- Stack/ReverseArray.java
+++ Stack/ReverseArray.java
@@ -1,9 +1,23 @@
 public class ReverseArray {
     public static void main(String[] args){
-        System
+        String stuff[] = new String[]{"I", "am", "infected", "with", "Coronavirus"};
+        print(stuff);
+        reverse(stuff);
+        print(stuff);
     }
-
-    public static <E> void (){
-
+    static <E extends Comparable<E>>void print (E arr[]){
+        for (int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+        
+    static <E extends Comparable<E>> void reverse(E arr[]){
+        Stack<E>  s = new Stack<E>();
+        for (int i = 0; i<arr.length; i++){
+            s.push(arr[i]);
+        }
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = s.pop();
+        }
     }
 }
